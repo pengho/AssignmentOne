b48d3d4369e2694957a2bece35189164768f6c66
ALittleOf <ngqirongrachel@gmail.com>
2021-02-07 16:19:37 +0800

    minor edits

--- index.js
+++ index.js
@@ -10,35 +10,31 @@ async function getData(){
     const response = await fetch(url);
     const data = await response.json();
     const {carpark_number, update_datetime, total_lots, lots_type, lots_available} = data;
-
-    let carparkData = data.items[0].carpark_data;
+    
+    var carparkData = data.items[0].carpark_data;
     console.log(carparkData);
-    let firstArray = carparkData[0]; //first object of the array
+    var firstArray = carparkData[0]; //first object of the array
     console.log(firstArray);
-    let firstObj = firstArray.carpark_info[0]
+    var firstObj = firstArray.carpark_info[0]
     console.log(firstObj);
-    console.log(firstArray.carpark_number);
+
+    /*var firstCaparkNum = firstArray.carpark_number;
+    console.log(firstArray.carpark_number);*/
+    document.createElement("firstCarparkNum").innerHTML=firstArray.carpark_number;
+
+    var firstUpdateDateTime = firstArray.update_datetime;
     console.log(firstArray.update_datetime);
+
+    var firstTotalLots = firstObj.total_lots;
     console.log(firstObj.total_lots);
+
+    var firstLotType = firstObj.lot_type;
     console.log(firstObj.lot_type);
+
+    var firstAvailable = firstObj.lots_available;
     console.log(firstObj.lots_available);
-
-
-
-    document.getElementById("dateTime").textContent = update_datetime;
-    document.getElementById("totalLots").textContent = total_lots;
-    document.getElementById("lotsType").textContent = lots_type;
-    document.getElementById("lotsAvailable").textContent = lots_available;           
+           
 }
-
-
-/*// this works, try this if anything else doesn't
-var getData = () => {
-    console.log ("Hello World");};
-
-getData();
-*/
-
 
 
 /*fetch('https://api.data.gov.sg/v1/transport/carpark-availability', {
@@ -52,18 +48,4 @@ getData();
 })
     .then(data => console.log(data))
     .catch(error => console.log("error"));
-*/
-
-
-
-/*
-Local Storage
-
-localStorage.getItem("Key Name");
-localStorage.setItem("Key Name", item that you want to store);
-localStorage.removeItem("Key Name");
-
-storingObject -> JSON.stringify(localStorage.getItem("Key Name"))
-
-
 */
