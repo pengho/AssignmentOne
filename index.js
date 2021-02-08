79924797ae11379155e6f5dd6a497550b05dc146
ALittleOf <ngqirongrachel@gmail.com>
2021-02-08 12:02:19 +0800

    little edits

--- index.js
+++ index.js
@@ -1,64 +1,10 @@
 // Assignment One (02.526 IDV)
+// Due: 25-Feb
 'use strict'
 
 window.onload = () => { // execute commands inside every time browser reloaded
     getData()
 }
-
-/*const url = "https://api.data.gov.sg/v1/transport/carpark-availability";
-var carparkData;
-
-async function getData(){
-    const response = await fetch(url);
-    const data = await response.json();
-    const {carpark_number, update_datetime, total_lots, lots_type, lots_available} = data;
-    
-    var carparkData = data.items[0].carpark_data;
-    console.log(carparkData);
-    // return (carparkData.length);
-    console.log(carparkData.length);
-    
-    var firstArray = carparkData[0]; //first object of the array
-    console.log(firstArray);
-    var firstObj = firstArray.carpark_info[0]
-    console.log(firstObj);
-
-
-
-    var firstCaparkNum = firstArray.carpark_number;
-    console.log(firstArray.carpark_number);
-
-    var firstUpdateDateTime = firstArray.update_datetime;
-    console.log(firstArray.update_datetime);
-
-    var firstTotalLots = firstObj.total_lots;
-    console.log(firstObj.total_lots);
-
-    var firstLotType = firstObj.lot_type;
-    console.log(firstObj.lot_type);
-
-    var firstAvailable = firstObj.lots_available;
-    console.log(firstObj.lots_available);           
-}*/
-
-
-/*function buildTable(){
-    var table = document.getElementById('myTable')
-
-    for (var i = 0; i < carparkData.length; i++){
-        var row = `<tr>
-                        <td>${data[i].carpark_number}</td>
-                        <td>${data[i].update_datetime}</td>
-                        <td>${data[i].lots_available}</td>
-                        <td>${data[i].lots_type}</td>
-                        <td>${data[i].total_lots}</td>
-                  </tr>`
-        table.innerHTML += row
-    }
-}
-buildTable(carparkData);*/
-
-
 
 async function getData() {
     try{
